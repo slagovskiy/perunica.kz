d0bfa8752e43ddbb3c20f2c7dae7efe7d00715c8
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-05-06 11:52:18 +0600

    media

--- www/perunica/urls.py
+++ www/perunica/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from perunica.settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^$', 'perunica.views.index', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': MEDIA_ROOT,
+    }),
+
 
     url(r'^admin/', include(admin.site.urls)),
 ]
