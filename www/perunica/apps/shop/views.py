7a935ad5763b52c7b09a0070bc5b18dd5861c81f
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-05-06 14:28:00 +0600

    auto select main menu

--- www/perunica/apps/shop/views.py
+++ www/perunica/apps/shop/views.py
@@ -9,14 +9,16 @@ log = logging.getLogger(__name__)
 
 def index(request):
     context = {}
-    return render(request, 'index.html', context)
+    return render(request, 'shop/index.html', context)
 
 
 def get_menu(request, menu_slug):
-    menu = Menu.objects.all().filter(slug=menu_slug)
-    items = []
-    context = {
-        'menu': menu,
-        'items': items
-    }
-    return render(request, 'index.html', context)
+    try:
+        menu = Menu.objects.all().filter(slug=menu_slug)[0]
+        context = {
+            'menu': menu
+        }
+    except:
+        context = {}
+        log.error('Error get_menu')
+    return render(request, 'shop/index.html', context)
