36b00b6ed3129e068011dbf592ee38708a152a3d
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-05-06 13:58:19 +0600

    fix submenu widget

--- www/perunica/utils/templatetags/widgets.py
+++ www/perunica/utils/templatetags/widgets.py
@@ -19,7 +19,7 @@ def widget_sub_menu(menu):
     try:
         from perunica.apps.shop.models import SubMenu
         from perunica.apps.shop.models import Menu
-        sub_menu = SubMenu.objects.filter(menu=menu).order_by('sort', 'name').exclude(deleted=True)
+        sub_menu = SubMenu.objects.filter(menu=menu).order_by('name').exclude(deleted=True)
     except:
         log.exception('Error get sub menu')
     return {'sub_menu': sub_menu}
