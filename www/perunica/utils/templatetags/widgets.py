a99a10c5e81cd94f52e803dee44903d00af11364
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-05-06 13:45:12 +0600

    submenu widget

--- www/perunica/utils/templatetags/widgets.py
+++ www/perunica/utils/templatetags/widgets.py
@@ -12,3 +12,14 @@ def widget_main_menu():
     except:
         log.exception('Error get main menu')
     return {'main_menu': main_menu}
+
+
+@register.inclusion_tag('widgets/sub_menu.html')
+def widget_sub_menu(menu):
+    try:
+        from perunica.apps.shop.models import SubMenu
+        from perunica.apps.shop.models import Menu
+        sub_menu = SubMenu.objects.filter(menu=menu).order_by('sort', 'name').exclude(deleted=True)
+    except:
+        log.exception('Error get sub menu')
+    return {'sub_menu': sub_menu}
