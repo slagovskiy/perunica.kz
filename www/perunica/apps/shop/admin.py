78b1da3b8931d392fe20f211e3c486e46a281432
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-05-06 13:35:20 +0600

    submenu admin

--- www/perunica/apps/shop/admin.py
+++ www/perunica/apps/shop/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, SubMenu
 
 
 class MenuAdmin(admin.ModelAdmin):
-    #fields = ['sort', 'slug', 'name', 'deleted']
     ordering = ['sort', 'name']
     list_display = ['slug', 'name', 'sort', 'deleted']
     list_filter = ['slug']
     search_fields = ['slug']
 
 
+class SubMenuAdmin(admin.ModelAdmin):
+    ordering = ['menu', 'name']
+    list_display = ['slug', 'menu', 'name', 'deleted']
+    list_filter = ['slug']
+    search_fields = ['slug']
+
+
 admin.site.register(Menu, MenuAdmin)
+admin.site.register(SubMenu, SubMenuAdmin)
