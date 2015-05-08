1403b973c6090b0b32a851109d8c4f37b1dfc674
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-05-08 15:19:02 +0600

    first page

--- www/perunica/views.py
+++ www/perunica/views.py
@@ -1,9 +1,17 @@
 import logging
 from django.shortcuts import render
 from django.http import HttpResponse
+from perunica.apps.shop.models import Goods
 
 log = logging.getLogger(__name__)
 
 def index(request):
-    context = {}
+    try:
+        goods = Goods.objects.all().filter(is_on_first=True, deleted=False)
+        context = {
+            'goods': goods
+        }
+    except:
+        context = {}
+        log.error('Error get_index')
     return render(request, 'index.html', context)
