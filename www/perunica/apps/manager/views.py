10010085ff0e663c388a7c4511885172ea6340d8
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-05-19 09:43:59 +0600

    manager login

--- www/perunica/apps/manager/views.py
+++ www/perunica/apps/manager/views.py
@@ -1,7 +1,7 @@
 import logging
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import User as UserManager
+from .models import Manager
 import uuid
 
 log = logging.getLogger(__name__)
@@ -10,10 +10,10 @@ log = logging.getLogger(__name__)
 def index(request):
     try:
         context = {}
-        if 'UserManager' not in request.session:
+        if 'Manager' not in request.session:
             return HttpResponseRedirect('/manager/login/')
         else:
-            if request.session['UserManager'] == None:
+            if request.session['Manager'] == None:
                 return HttpResponseRedirect('/manager/login/')
     except:
         context = {}
@@ -23,6 +23,15 @@ def index(request):
 
 def login(request):
     try:
+        if 'username' in request.POST and 'password' in request.POST:
+            u = Manager.objects.filter(login=request.POST['username'], password=request.POST['password'], deleted=False)
+            if len(u)>0:
+                request.session['Manager'] = {
+                    'name': u[0].name,
+                    'login': u[0].login,
+                    'email': u[0].email
+                }
+                HttpResponseRedirect('/manager/')
         context = {}
     except:
         context = {}
