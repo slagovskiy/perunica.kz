dbb3e3274f64b9cb83be4b358d0a3999585d36ed
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-05-20 14:48:20 +0600

    feedback admin

--- www/perunica/apps/feedback/admin.py
+++ www/perunica/apps/feedback/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+from .models import Feedback
 
+
+class FeedbackAdmin(admin.ModelAdmin):
+    ordering = ['date', 'name']
+    list_display = ['date', 'name', 'allowed', 'deleted']
+
+
+admin.site.register(Feedback, FeedbackAdmin)
