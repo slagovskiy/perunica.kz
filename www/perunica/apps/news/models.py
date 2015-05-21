1e8ba09b91cb57b13961421d24b748cdf4ab3ab5
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-05-21 16:37:41 +0600

    news model

--- www/perunica/apps/news/models.py
+++ www/perunica/apps/news/models.py
@@ -5,7 +5,7 @@ import os
 
 class News(models.Model):
     deleted = models.BooleanField(default=False, verbose_name=u'Deleted')
-    date = models.DateTimeField(auto_now_add=True, verbose_name=u'Date')
+    date = models.DateTimeField(verbose_name=u'Date')
     title = models.CharField(max_length=255, default=u'', verbose_name=u'Title')
     message = models.TextField(default=u'', null=True, blank=True, verbose_name=u'Message')
 
