53eeca5eea10f00314d130221dd5001222911c63
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-23 15:44:42 +0600

    SU admin

--- www/perunica/utils/admin.py
+++ www/perunica/utils/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
-from .models import Global, Image
+from .models import Global, Image, SU
 
 
 class GlobalAdmin(admin.ModelAdmin):
     ordering = ['active']
     list_display = ['active', 'meta_title']
 
+
+class SUAdmin(admin.ModelAdmin):
+    ordering = ['active']
+    list_display = ['active', 'name']
+
+
 class ImageAdmin(admin.ModelAdmin):
     ordering = ['name']
     readonly_fields = ['code', 'admin_image']
     list_display = ['name', 'admin_image_list', 'code']
 
+
 admin.site.register(Global, GlobalAdmin)
+admin.site.register(SU, SUAdmin)
 admin.site.register(Image, ImageAdmin)
